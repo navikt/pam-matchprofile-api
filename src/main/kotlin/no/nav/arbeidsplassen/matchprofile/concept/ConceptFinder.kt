6d63f231d2b5c28639e758f06ea27d3baf27abb8
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-02-28 08:24:13 +0100

    introduced concept finder

---
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/concept/ConceptFinder.kt
@@ -0,0 +1,141 @@
+package no.nav.arbeidsplassen.matchprofile.concept
+
+import jakarta.inject.Singleton
+import no.nav.arbeidsplassen.matchprofile.cv.CvDTO
+import no.nav.arbeidsplassen.matchprofile.event.EventDTO
+import no.nav.arbeidsplassen.matchprofile.janzz.*
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.education
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.experience
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.function
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.industry
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.occupation
+import no.nav.arbeidsplassen.matchprofile.job.AdDTO
+import no.nav.arbeidsplassen.matchprofile.profile.Concept
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.skill
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.softskill
+import no.nav.arbeidsplassen.matchprofile.janzz.Branch.specialization
+import org.slf4j.LoggerFactory
+
+@Singleton
+class ConceptFinder(private val janzzClient: JanzzClient) {
+
+    private val cSkills = 10
+    private val cExpansions = 10
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ConceptFinder::class.java)
+    }
+
+    fun findConceptsForJobAd(ad:AdDTO, related: Boolean = true) : List<Concept> {
+        val concepts = getParsedConcepts(parseJobAd(ad))
+        LOG.info("parsed: ${ad.uuid} found ${concepts.size} concepts")
+        return if (related ) concepts.plus(getRelatedConcepts(concepts)) else concepts
+    }
+
+    fun findKnownConceptsForText(text: String, related: Boolean = true) : List<Concept> {
+        val concepts = getParsedConcepts(parseText(text))
+        return if (related ) concepts.plus(getRelatedConcepts(concepts)) else concepts
+    }
+
+    fun findKnownConcepts(knownConcepts: List<Concept>, related: Boolean = true) : List<Concept> {
+        val concepts = getParsedConcepts(
+            parseText(knownConcepts.filter { it.cid != null }.map { it.label }.joinToString("\n"))
+        )
+        return if (related) concepts.plus(getRelatedConcepts(concepts)) else concepts
+    }
+
+    private fun parseText(text: String): ParsedDTO {
+        return janzzClient.parseJob(QueryBody("", body = text))
+    }
+
+    private fun parseJobAd(ad: AdDTO): ParsedDTO {
+        val jobtitle = ad.properties["jobtitle"]?:""
+        val text =  ad.properties["adtext"]?.toString()?.replace(Regex("<.*?>")," ") ?: " "
+        return janzzClient.parseJob(QueryBody(title = "${ad.title} $jobtitle", body = text))
+    }
+
+    private fun parseJobAds(ads: List<AdDTO>): List<ParsedDTO> {
+        return ads.map { parseJobAd(it)}
+    }
+
+    private fun parseCV(cv: CvDTO): ParsedDTO {
+        val exp = cv.workExperience.map {"${it.jobTitle?:""} ${it.alternativeJobTitle?:""} ${it.description?:""}"}
+        val edu = cv.education.map { "${it.institution?:""} ${it.field?:""} ${it.description?:""}" }
+        val skills = cv.skills.map { it.title?:"" }
+        val body = exp.joinToString(" ") + edu.joinToString(" ") + skills.joinToString(" ")
+        val query = QueryBody("", body)
+        return janzzClient.parseJob(query)
+    }
+
+    private fun parseCVs(cvs: List<CvDTO>): List<ParsedDTO> {
+        return cvs.map { parseCV(it) }
+    }
+
+    private fun parseEvent(event: EventDTO): ParsedDTO {
+        val query = QueryBody(event.title, event.description)
+        return janzzClient.parseJob(query)
+    }
+
+    private fun parseEvents(events: List<EventDTO>): List<ParsedDTO> {
+        return events.map { parseEvent(it) }
+    }
+
+    private fun getParsedConcepts(doc: ParsedDTO): List<Concept> {
+        val concepts = termsToConcepts(doc.json.Occupation, occupation)
+            .plus(termsToConcepts(doc.json.Skills, skill))
+            .plus(termsToConcepts(doc.json.Education, education))
+            .plus(termsToConcepts(doc.json.Experience, experience))
+            .plus(termsToConcepts(doc.json.Softskills, softskill))
+            .plus(termsToConcepts(doc.json.Specialization, specialization))
+            .plus(termsToConcepts(doc.json.Function, function))
+            .plus(termsToConcepts(doc.json.Industry, industry))
+        return concepts
+    }
+
+    private fun getRelatedConcepts(concepts: List<Concept>): List<Concept> {
+        val onlyOccupations = concepts
+            .filter { it.branch == occupation }
+            .filter { it.cid != null }
+
+        val moreSkills = onlyOccupations
+            .map { Pair(it.label, suggestBranchRelation(it.label, skill)) }
+            .flatMap {(label, results) ->
+                results
+                    .take(cSkills)
+                    .map { Concept(label=it.label, cid = it.cid, branch = skill, expandedConcept = label)}
+             }
+
+        val expansions = onlyOccupations
+            .map { Pair(it.label, janzzClient.expandConceptByCid(it.cid!!).results ) }
+            .flatMap {(label, results) -> results
+                                            .take(cExpansions)
+                                            .map{ Concept(label=it.label, cid=it.cid, branch=occupation, expandedConcept = label)}}
+
+        return moreSkills.plus(expansions)
+    }
+
+    private fun termsToConcepts(terms:List<List<String>>, type: String): List<Concept> {
+        return terms.map { toConcept(it,type) }
+    }
+
+    private fun toConcept(terms: List<String?>, type: String): Concept {
+        println("$type:$terms")
+        return Concept(
+            label = terms[0]!!,
+            cid = terms[1]?.toLong(),
+            branch = type
+        )
+    }
+
+    fun suggestBranchRelation(occupation: String, relation: String): List<ConceptLabelDTO> {
+
+        return try {
+            janzzClient.occupationBranchRelation(occupation, relation ).results
+        }
+        catch (e:Exception) {
+            LOG.error("Got exception while calling suggestBranchRelation for $occupation", e)
+            emptyList<ConceptLabelDTO>()
+        }
+    }
+
+}
---
+++ src/test/kotlin/no/nav/arbeidsplassen/matchprofile/concept/ConceptFinderIT.kt
@@ -0,0 +1,22 @@
+package no.nav.arbeidsplassen.matchprofile.concept
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import no.nav.arbeidsplassen.matchprofile.job.AdDTO
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class ConceptFinderIT(private val conceptFinder: ConceptFinder, private val objectMapper: ObjectMapper) {
+
+    //@Test
+    fun findConceptsForJob() {
+        val ad = objectMapper.readValue(ConceptFinder::class.java.classLoader.getResourceAsStream("jobs/bartender.json"), AdDTO::class.java)
+        println(objectMapper.writeValueAsString(conceptFinder.findConceptsForJobAd(ad)))
+    }
+
+    @Test
+    fun findConceptsForText() {
+        val text = "Servitører\n Bartendere\n Servitør\n Serviceinnstilt\n Blid\n Omgjengelig\n Kommunikasjonsevner\n Øltapper"
+        println(objectMapper.writeValueAsString(conceptFinder.findKnownConceptsForText(text, false)))
+    }
+}
