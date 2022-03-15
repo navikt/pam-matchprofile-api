570e7784ef2c8ba9f19d5a9901a08a11a417ce4f
ahandeland-nav <alexander.hadeland@nav.no>
2022-03-15 18:03:24 +0100

    Use ad to matchprofile functionality for kafka events

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/AdKafkaListener.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/AdKafkaListener.kt
@@ -3,11 +3,17 @@ package no.nav.arbeidsplassen.matchprofile.job
 import io.micronaut.configuration.kafka.annotation.KafkaListener
 import io.micronaut.configuration.kafka.annotation.Topic
 import io.micronaut.context.annotation.Requires
+import no.nav.arbeidsplassen.matchprofile.profile.MatchProfileMaker
+import no.nav.arbeidsplassen.matchprofile.profile.MatchProfileService
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 
 @Requires(property = "ad.kafka.enabled", value = "true")
 @KafkaListener
-class AdKafkaListener {
+class AdKafkaListener(
+    private val matchProfileMaker: MatchProfileMaker,
+    private val matchProfileService: MatchProfileService
+) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(AdKafkaListener::class.java)
@@ -15,7 +21,20 @@ class AdKafkaListener {
 
     @Topic("\${ad.kafka.topic}")
     fun receiveAd(records: List<AdDTO>, offsets: List<Long>) {
-        LOG.info("Received ${records.size} from Kafka")
+        LOG.info("Received $records from Kafka")
 
+        val saved = records.map { handleRecord(it) }
+
+        LOG.info("Handled ${saved.size} records successfully")
+    }
+
+    private fun handleRecord(ad: AdDTO): Boolean {
+        return try {
+            val matchProfileDTO = matchProfileMaker.jobMatchProfile(ad)
+            matchProfileService.save(matchProfileDTO)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
