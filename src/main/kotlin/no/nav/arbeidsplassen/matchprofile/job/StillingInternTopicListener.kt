563a8d851d39a0663c2a29f90de7ead4a038ec90
ahandeland-nav <alexander.hadeland@nav.no>
2022-03-17 16:24:56 +0100

    Error handling with KafkaExceptionHandler

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/StillingInternTopicListener.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/StillingInternTopicListener.kt
@@ -9,10 +9,11 @@ import no.nav.arbeidsplassen.matchprofile.profile.MatchProfileService
 import org.slf4j.LoggerFactory
 
 @Requires(property = "ad.kafka.enabled", value = "true")
-@KafkaListener
+@KafkaListener(batch = true)
 class StillingInternTopicListener(
     private val matchProfileMaker: MatchProfileMaker,
-    private val matchProfileService: MatchProfileService
+    private val matchProfileService: MatchProfileService,
+    private val kafkaStateRegistry: KafkaStateRegistry
 ) {
 
     companion object {
@@ -20,16 +21,21 @@ class StillingInternTopicListener(
     }
 
     @Topic("\${ad.kafka.topic}")
-    fun syncAds(adDtos: List<AdDTO>, offsets: List<Long>): List<MatchProfileDTO> {
-        LOG.info("Received ${adDtos.size} ads from Kafka")
+    fun syncAds(ads: List<AdDTO>, offsets: List<Long>): List<MatchProfileDTO> {
+        LOG.info("Received ${ads.size} ads from Kafka")
 
-        val saved = adDtos
+        if (kafkaStateRegistry.hasError()) {
+            LOG.error("Kafka is in an error state. Skipping batch to avoid message oss. Consumer should be paused.")
+            return emptyList()
+        }
+
+        val storedProfiles = ads
             .filter { it.publishedByAdmin != null }
             .map { ad -> matchProfileMaker.jobMatchProfile(ad) }
             .map { matchProfile -> matchProfileService.save(matchProfile) }
 
-        LOG.info("Stored ${saved.size} match profiles successfully")
-        return saved
+        LOG.info("Stored ${storedProfiles.size} match profiles successfully")
+        return storedProfiles
     }
 
 }
