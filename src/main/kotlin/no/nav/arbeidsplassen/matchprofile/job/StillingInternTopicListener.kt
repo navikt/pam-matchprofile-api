a3a726f34808fd9a42e867e20263a46e303656a4
ahandeland-nav <alexander.hadeland@nav.no>
2022-03-18 11:15:01 +0100

    Disable autocommit

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/StillingInternTopicListener.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/StillingInternTopicListener.kt
@@ -1,15 +1,17 @@
 package no.nav.arbeidsplassen.matchprofile.job
 
 import io.micronaut.configuration.kafka.annotation.KafkaListener
+import io.micronaut.configuration.kafka.annotation.OffsetStrategy
 import io.micronaut.configuration.kafka.annotation.Topic
 import io.micronaut.context.annotation.Requires
 import no.nav.arbeidsplassen.matchprofile.profile.MatchProfileDTO
 import no.nav.arbeidsplassen.matchprofile.profile.MatchProfileMaker
 import no.nav.arbeidsplassen.matchprofile.profile.MatchProfileService
+import org.apache.kafka.clients.consumer.Consumer
 import org.slf4j.LoggerFactory
 
 @Requires(property = "ad.kafka.enabled", value = "true")
-@KafkaListener(batch = true)
+@KafkaListener(batch = true, offsetStrategy = OffsetStrategy.DISABLED)
 class StillingInternTopicListener(
     private val matchProfileMaker: MatchProfileMaker,
     private val matchProfileService: MatchProfileService,
@@ -21,7 +23,7 @@ class StillingInternTopicListener(
     }
 
     @Topic("\${ad.kafka.topic}")
-    fun syncAds(ads: List<AdDTO>, offsets: List<Long>): List<MatchProfileDTO> {
+    fun syncAds(ads: List<AdDTO>, offsets: List<Long>, kafkaConsumer: Consumer<*, *>): List<MatchProfileDTO> {
         LOG.info("Received ${ads.size} ads from Kafka")
 
         if (kafkaStateRegistry.hasError()) {
@@ -33,6 +35,8 @@ class StillingInternTopicListener(
             .filter { it.publishedByAdmin != null }
             .map { ad -> matchProfileMaker.jobMatchProfile(ad) }
             .map { matchProfile -> matchProfileService.save(matchProfile) }
+
+        kafkaConsumer.commitSync()
 
         LOG.info("Stored ${storedProfiles.size} match profiles successfully")
         return storedProfiles
