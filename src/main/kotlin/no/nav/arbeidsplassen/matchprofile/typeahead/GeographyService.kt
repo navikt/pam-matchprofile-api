5b74e6f1e3f384153e77b70248acd87ab2117054
ahandeland-nav <alexander.hadeland@nav.no>
2022-03-24 14:32:54 +0100

    Refactoring

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/typeahead/GeographyService.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/typeahead/GeographyService.kt
@@ -2,27 +2,23 @@ package no.nav.arbeidsplassen.matchprofile.typeahead
 
 import jakarta.inject.Singleton
 import no.nav.pam.geography.County
+import no.nav.pam.geography.CountyDAO
 import no.nav.pam.geography.Municipality
-import no.nav.pam.geography.PostDataDAO
+import no.nav.pam.geography.MunicipalityDAO
 
 @Singleton
 class GeographyService {
 
-    companion object {
-        private val postDataDAO = PostDataDAO()
+    fun getTypeaheadCounties(search: String, numResults: Int?): List<County> {
+        return CountyDAO.getImmutableCountySet()
+            .filter { it.name.contains(search, ignoreCase = true) }
+            .take(numResults ?: 20)
     }
 
-    fun getTypeaheadMunicipality(search: String): List<Municipality> {
-        return postDataDAO.allMunicipalities
-            .filter { it.name.contains(search, true) }
-    }
-
-    fun getTypeaheadCounty(search: String): List<County> {
-        return postDataDAO.allPostData
-            .map { it.county }
-            .toSet()
-            .toList()
-            .filter { it.name.contains(search, true) }
+    fun getTypeaheadMunicipalities(search: String, numResults: Int?): List<Municipality> {
+        return MunicipalityDAO().allMunicipalities
+            .filter { it.name.contains(search, ignoreCase = true) }
+            .take(numResults ?: 20)
     }
 
 }
--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/typeahead/TypeaheadController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/typeahead/TypeaheadController.kt
@@ -4,19 +4,27 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
 import no.nav.pam.geography.*
+import org.slf4j.LoggerFactory
 
 @Controller("/api/v1/typeahead")
 class TypeaheadController(private val geographyService: GeographyService) {
 
+    companion object {
+        private val LOG = LoggerFactory.getLogger(TypeaheadController::class.java)
+    }
 
     @Get("/geografi/kommune")
-    fun typeaheadKommune(@QueryValue q: String): List<Municipality> {
-        return geographyService.getTypeaheadMunicipality(q)
+    fun typeaheadKommune(@QueryValue q: String, @QueryValue numResults: Int?): List<Municipality> {
+        LOG.info("Received request for typeahead kommune, query: $q")
+
+        return geographyService.getTypeaheadMunicipalities(q, numResults)
     }
 
     @Get("/geografi/fylke")
-    fun typeaheadFylke(@QueryValue q: String): List<County> {
-        return geographyService.getTypeaheadCounty(q)
+    fun typeaheadFylke(@QueryValue q: String, @QueryValue numResults: Int?): List<County> {
+        LOG.info("Received request for typeahead fylke, query: $q")
+
+        return geographyService.getTypeaheadCounties(q, numResults)
     }
 
 }
