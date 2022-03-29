81ae77952c732c881b19e41fd28bb84dcdce4e39
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-03-29 13:59:03 +0200

    checking janzz

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/health/StatusController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/health/StatusController.kt
@@ -3,10 +3,11 @@ package no.nav.arbeidsplassen.matchprofile.health
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import no.nav.arbeidsplassen.matchprofile.profile.ConceptFinder
 import org.slf4j.LoggerFactory
 
 @Controller("/internal")
-class StatusController() {
+class StatusController(private val conceptFinder: ConceptFinder) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(StatusController::class.java)
@@ -21,4 +22,12 @@ class StatusController() {
     fun isAlive(): HttpResponse<String> {
         return HttpResponse.ok("OK")
     }
+
+    @Get("/check")
+    fun check(): HttpResponse<String> {
+        LOG.info("Checking janzz")
+        conceptFinder.findConceptsForText("Dette er en test")
+        return HttpResponse.ok("OK")
+    }
+
 }
