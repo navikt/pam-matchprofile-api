f014b38c4caccf2365fbaa598b50c6680e8615be
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-04-08 15:02:07 +0200

    disable kafka for a moment, too slow

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/health/StatusController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/health/StatusController.kt
@@ -3,12 +3,11 @@ package no.nav.arbeidsplassen.matchprofile.health
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import no.nav.arbeidsplassen.matchprofile.job.KafkaStateRegistry
 import no.nav.arbeidsplassen.matchprofile.profile.ConceptFinder
 import org.slf4j.LoggerFactory
 
 @Controller("/internal")
-class StatusController(private val conceptFinder: ConceptFinder, private val kafkaStateRegistry: KafkaStateRegistry) {
+class StatusController(private val conceptFinder: ConceptFinder) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(StatusController::class.java)
@@ -21,9 +20,6 @@ class StatusController(private val conceptFinder: ConceptFinder, private val kaf
 
     @Get("/isAlive")
     fun isAlive(): HttpResponse<String> {
-        if (kafkaStateRegistry.hasError()) {
-            return HttpResponse.serverError("Kafka state is error")
-        }
         return HttpResponse.ok("OK")
     }
 
