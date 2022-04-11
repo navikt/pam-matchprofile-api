24a49140f1917c077e7a663675e0817eae4a9871
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-04-11 10:44:52 +0200

    janzz is too slow, need to do it one by one

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/StillingInternTopicListener.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/job/StillingInternTopicListener.kt
@@ -12,7 +12,7 @@ import org.apache.kafka.clients.consumer.Consumer
 import org.slf4j.LoggerFactory
 
 @Requires(property = "ad.kafka.enabled", value = "true")
-@KafkaListener(batch = true, offsetStrategy = OffsetStrategy.DISABLED, offsetReset = OffsetReset.LATEST)
+@KafkaListener(offsetStrategy = OffsetStrategy.DISABLED, offsetReset = OffsetReset.LATEST)
 class StillingInternTopicListener(
     private val matchProfileMaker: MatchProfileMaker,
     private val matchProfileService: MatchProfileService,
@@ -24,27 +24,20 @@ class StillingInternTopicListener(
     }
 
     @Topic("\${ad.kafka.topic}")
-    fun syncAds(ads: List<AdDTO>, offsets: List<Long>, kafkaConsumer: Consumer<*, *>): List<MatchProfileDTO> {
-        LOG.info("Received ${ads.size} ads from Kafka")
+    fun syncAds(ad: AdDTO, offset: Long, kafkaConsumer: Consumer<*, *>) {
+        LOG.info("Received ad ${ad.uuid} from Kafka")
 
         if (kafkaStateRegistry.hasError()) {
-            LOG.error("Kafka is in an error state. Skipping batch to avoid message loss. Consumer should be paused.")
-            return emptyList()
+            LOG.error("Kafka is in an error state. Skipping to avoid message loss. Consumer should be paused.")
+            return
         }
-
-        val storedProfiles = ads.asSequence()
-            .sortedByDescending(AdDTO::updated)
-            .distinctBy(AdDTO::uuid)
-            .filter { it.publishedByAdmin != null }
-            .map { ad -> matchProfileMaker.jobMatchProfile(ad) }
-            .map { matchProfile -> matchProfileService.save(matchProfile) }.toList()
-
-        LOG.info("committing at offset ${offsets.last()}")
+        if (ad.publishedByAdmin != null) {
+            val matchProfile = matchProfileMaker.jobMatchProfile(ad)
+            matchProfileService.save(matchProfile)
+        }
+        LOG.info("committing at offset $offset")
         kafkaConsumer.commitSync()
-
-        LOG.info("Stored ${storedProfiles.size} match profiles successfully")
         kafkaStateRegistry.clearStateRegistry()
-        return storedProfiles
     }
 
 }
